c46ff7b84ba02e5d5baf62ab2fad833f1bac6fff
tig04273 <tig04273@naver.com>
2023-02-14 16:08:50 +0900

    manual main ui
    Main UI

---
+++ CoH2Manual/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoH2Manual
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //WindowStyle = WindowStyle.None;
+            //AllowsTransparency = true;
+            this.SizeToContent = SizeToContent.Manual;
+            InitializeComponent();
+        }
+
+        private void Sov_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Window soviet_Form = new SovietForm();
+            //soviet_Form.Show();
+            Frame_CoH2Manual.Source = new Uri("Soviet\\SovietMain.xaml", UriKind.Relative);
+            
+        }
+
+        private void USA_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ENG_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GER_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OKW_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Translate_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CoH2Manual/Soviet/SovietMain.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoH2Manual.Soviet
+{
+    /// <summary>
+    /// SovietMain.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SovietMain : Page
+    {
+        public SovietMain()
+        {
+            InitializeComponent();
+            Console.WriteLine("helloworld");
+        }
+    }
+}
